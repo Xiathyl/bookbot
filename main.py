6a6d656dba7b076dfc0f0e302d583fc9052b2721
Xiathyl <stever445@gmail.com>
2025-02-25 21:31:27 +0000

    Project finished. Usage: python3 main.py <path_to_book_txt_file>

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from stats import count_words
+from stats import count_letters
+import sys
 
 def get_book_text():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_location = sys.argv[1]
+    if book_location == None:
+        print("No location")
+        pass
+    with open(book_location) as f:
         file_contents = f.read()
-        count_words(file_contents)
-    pass
+        words = count_words(file_contents)
+        letters = count_letters(file_contents)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_location}")
+        print("----------- Word Count ----------")
+        print(f"Found {words} total words")
+        print("--------- Character Count -------")
+        for i in letters:
+            print(f"{i}: {letters[i]}")
+        print("============= END ===============")
 
 get_book_text()
--- stats.py
+++ stats.py
@@ -2,4 +2,20 @@ def count_words(text):
     counter = 0
     for i in text.split():
         counter = counter +1
-    print(f"{counter} words found in the document")
+    return counter
+    #print(f"{counter} words found in the document")
+
+def sort_on():
+    return dict.values()
+
+def count_letters(book):
+    letters = {'t':0}
+    for i in book:
+        l_letter= i.lower()
+        if l_letter not in letters and l_letter.isalpha() == True:
+            letters[l_letter] = l_letter
+            letters[l_letter] = 1
+        elif l_letter in letters and l_letter.isalpha() == True:
+            letters[l_letter] += 1
+    sorted_letters = dict(sorted(letters.items(),key=lambda item: item[1], reverse=True))
+    return(sorted_letters)
